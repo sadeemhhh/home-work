1df60cb9c0ab6b77454ce8d2501cfee0abf43391
sadeem <pinkkfloww@gmail.com>
2024-12-04 14:12:45 +0300

    first commit

---
+++ hello.dart
@@ -0,0 +1,20 @@
+void main (){
+int age = 16;
+double salary = 20000;
+String id = "33333";
+bool ISstudent= true;
+
+var number = 22;
+number = 70;
+
+number = 55.5;
+
+dynamic name = "sss";
+ name = "hamda";
+ name = 44;
+
+ //dynamic change swith anything its like a sheep 
+
+
+    print("hell0 all");
+}
---
+++ test.dart
@@ -0,0 +1,10 @@
+class test{
+  String name;
+  int age;
+
+  test(this.name, this.age);
+
+  void greet() {
+    print('Hello, my name is $name and I am $age years old.');
+  }
+}
